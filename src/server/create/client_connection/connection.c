5b4937d9223f1802ef70ce4a30144cda3c5979d5
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-01 12:09:10 +0200

    Add a function to check if the clients disconnected

--- src/server/create/client_connection/connection.c
+++ src/server/create/client_connection/connection.c
@@ -7,6 +7,18 @@
 
 #include "./../../../../include/teams_server.h"
 
+int *check_disconnection(steams_t *server, int i)
+{
+    int *clients = server->client_fds;
+
+    if (read(clients[i], server->buffer, 1024) == 0) {
+        printf("Client disconnected, socket fd is %d\n", clients[i]);
+        clients[i] = 0;
+    } else {
+    }
+    return (clients);
+}
+
 void infinite_loop(steams_t *server)
 {
     printf("Waiting for clients connections...\n");
