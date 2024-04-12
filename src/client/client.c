218b85d99e942ee2bcf042ce6d8b1cfbfc098549
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-12 20:13:33 +0200

    Add a function to read the input stream

--- src/client/client.c
+++ src/client/client.c
@@ -17,6 +17,17 @@ int server_connection(cteams_t *client)
     return (0);
 }
 
+void read_input(cteams_t *client)
+{
+    read(INPUT_FD, client->in_buffer, 1024);
+    if (strncmp(client->in_buffer, "/logout", 7) == 0){
+        close(client->fd);
+        to_exit(client);
+        client_event_logged_out("121", "No one");
+        exit(0);
+    }
+}
+
 int infinite_loop(cteams_t *client)
 {
     if (server_connection(client) == 84)
@@ -31,6 +42,7 @@ int infinite_loop(cteams_t *client)
         }
         send(client->fd, client->welcome, strlen(client->welcome), 0);
         if (FD_ISSET(INPUT_FD, &client->readfds)) {
+            read_input(client);
         } else if (FD_ISSET(client->fd, &client->readfds)) {
             read(client->fd, client->buffer, 1024);
             printf("%s\n", client->buffer);
