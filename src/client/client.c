325d3da335dfa95fa8014820312b20554d28539e
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-12 20:10:01 +0200

    Add a function which is handling with the client's main loop

--- src/client/client.c
+++ src/client/client.c
@@ -16,3 +16,25 @@ int server_connection(cteams_t *client)
     }
     return (0);
 }
+
+int infinite_loop(cteams_t *client)
+{
+    if (server_connection(client) == 84)
+        return(84);
+    while (1) {
+        FD_ZERO(&client->readfds);
+        FD_SET(client->fd, &client->readfds);
+        FD_SET(INPUT_FD, &client->readfds);
+        if (select(client->fd + 1, &client->readfds, NULL, NULL, NULL) < 0) {
+            printf("Error while selecting\n");
+            return(84);
+        }
+        send(client->fd, client->welcome, strlen(client->welcome), 0);
+        if (FD_ISSET(INPUT_FD, &client->readfds)) {
+        } else if (FD_ISSET(client->fd, &client->readfds)) {
+            read(client->fd, client->buffer, 1024);
+            printf("%s\n", client->buffer);
+        }
+    }
+    return(0);
+}
