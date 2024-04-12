8bb048b3ba60f4179f330e01f4b9a392bef63f0c
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-12 20:17:01 +0200

    Add a function to read files

---
+++ src/file_management/read_file.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams
+** File description:
+** read_file
+*/
+
+#include "../../include/my_teams.h"
+
+char *readfile(char *filepath)
+{
+    int size = 1024;
+    char *bf = malloc(sizeof(char) * size + 1);
+    int rd = 0;
+    int fd = open(filepath, O_RDONLY);
+
+    if (fd == -1){
+        return (NULL);
+    }
+    rd = read(fd, bf, size);
+    bf[rd] = '\0';
+    close(fd);
+    return(bf);
+}
