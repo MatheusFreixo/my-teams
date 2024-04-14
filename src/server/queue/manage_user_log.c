616c190b97dae7c96169663ec5d78e90f11e14a7
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-14 02:41:52 +0200

    Add functions to handle with the client's login/logout

---
+++ src/server/queue/manage_user_log.c
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams
+** File description:
+** manage_queue
+*/
+
+#include "../../../include/teams_server.h"
+
+bool check_client(steams_t *server, int client_fd)
+{
+    users_t *tmp = NULL;
+    LIST_FOREACH(tmp, &server->users, entry){
+        if (tmp->fd == client_fd){
+            return (true);
+        }
+    }
+    return (false);
+}
+
+char *get_user_name(steams_t *server, int client_fd)
+{
+    users_t *tmp = NULL;
+    LIST_FOREACH(tmp, &server->users, entry){
+        if (tmp->fd == client_fd){
+            return (tmp->name);
+        }
+    }
+    return (NULL);
+}
+
+int change_user_status(steams_t *server, char *name, int client_fd, bool expected_status)
+{
+    users_t *tmp = NULL;
+    LIST_FOREACH(tmp, &server->users, entry){
+        if (strcmp(tmp->name, name) == 0){
+            if (tmp->status == expected_status){
+                tmp->status = !tmp->status;
+            }
+            return (1);
+        }
+    }
+    return (0);
+}
+
+void add_user_to_list(steams_t *server, char *name, int client_fd)
+{
+    users_t *user = malloc(sizeof(users_t));
+
+    user->name = strdup(name);
+    user->status = true;
+    user->fd = client_fd;
+    user->id = gen_uuid_parsed();
+    if (LIST_EMPTY(&server->users)){
+        LIST_INSERT_HEAD(&server->users, user, entry);
+    } else {
+        LIST_INSERT_AFTER(server->last_user, user, entry);
+    }
+    server_event_user_created(user->id, user->name);
+    server->last_user = user;
+    users_t *tmp = LIST_FIRST(&server->users);
+}
+
+void user_log_in(steams_t *server, char *name, int client_fd)
+{
+    if (change_user_status(server, name, client_fd, false) == 0){
+        add_user_to_list(server, name, client_fd);
+    } else {
+        server_event_user_logged_in(get_user_id(server, name));
+    }
+    char *msg = malloc(sizeof(char) * 43);
+    char *id = strdup(get_user_id(server, name));
+    strcpy(msg, "LOGIN ");
+    strcat(msg, id);
+    write(client_fd, msg, 42);
+}
+
+void user_log_out(steams_t *server, int client_fd)
+{
+    char *name = get_user_name(server, client_fd);
+    name = get_user_name(server, client_fd);
+
+    if (change_user_status(server, name, client_fd, true) == 1){
+        server_event_user_logged_out(get_user_id(server, name));
+        char *msg = "LOGOUT";
+        write(client_fd, "LOGOUT", 6);
+    }
+}
