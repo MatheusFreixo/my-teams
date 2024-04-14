d9fcc503c33cc63a155faa897dfd1b8dd5851020
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-15 00:20:32 +0200

    Add functions to check if a respective channel, thread or team exists on their lists

--- src/server/queue/checkers.c
+++ src/server/queue/checkers.c
@@ -7,6 +7,42 @@
 
 #include "../../../include/teams_server.h"
 
+bool check_channel(steams_t *server, char *channel_id)
+{
+    channels_t *tmp = NULL;
+
+    LIST_FOREACH(tmp, &server->channels, entry){
+        if (strcmp(tmp->id, channel_id) == 0){
+            return (true);
+        }
+    }
+    return (false);
+}
+
+bool check_thread(steams_t *server, char *thread_id)
+{
+    threads_t *tmp = NULL;
+
+    LIST_FOREACH(tmp, &server->threads, entry){
+        if (strcmp(tmp->id, thread_id) == 0){
+            return (true);
+        }
+    }
+    return (false);
+}
+
+bool check_team(steams_t *server, char *name)
+{
+    teams_t *tmp = NULL;
+
+    LIST_FOREACH(tmp, &server->teams, entry){
+        if (strcmp(tmp->name, name) == 0){
+            return (true);
+        }
+    }
+    return (false);
+}
+
 bool check_client(steams_t *server, int client_fd)
 {
     users_t *tmp = NULL;
