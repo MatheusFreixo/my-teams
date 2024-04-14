7d010369e846ad3198d99f01113b7015af9c36d4
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-15 00:30:49 +0200

    Add a function to handle with the context sent by the command use

---
+++ src/server/commands/manage_context.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams
+** File description:
+** manage_context
+*/
+
+#include "./../../../include/teams_server.h"
+
+void manage_context(steams_t *server, char **command, int client_fd)
+{
+    server->team_id = NULL;
+    server->channel_id = NULL;
+    server->thread_id = NULL;
+    for (int i = 0; command[i] != NULL; i++) {
+        check_team(server, command[i]) ? server->team_id = command[i] : NULL;
+        check_channel(server, command[i]) ? server->channel_id = command[i] : NULL;
+        check_thread(server, command[i]) ? server->thread_id = command[i] : NULL;
+    }
+    if (server->team_id == NULL && server->channel_id == NULL && server->thread_id == NULL) {
+        server->context = false;
+    }
+}
