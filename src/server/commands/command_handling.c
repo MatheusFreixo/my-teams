df063b54479a2465fafef794f62554fedd93df0a
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-15 04:13:00 +0200

    Add more conditions to check the commands in the server

--- src/server/commands/command_handling.c
+++ src/server/commands/command_handling.c
@@ -12,8 +12,12 @@ int check_command(steams_t *server, char *command, int client_fd)
     char *tok = strtok(server->buffer, "\r\n");
     char **cmd = split_str(command, ' ');
 
-    if (check_log(cmd[0])) {
+    if (check_log(cmd[0])){
         manage_log_command(server, cmd[0], cmd[1], client_fd);
+    } else if (strcmp(cmd[0], "/use") == 0){
+        manage_context(server, cmd, client_fd);
+    } else {
+        manage_create_command(server, cmd, client_fd);
     }
     free(server->buffer);
     server->buffer = malloc(sizeof(char) * 1024 + 1);
