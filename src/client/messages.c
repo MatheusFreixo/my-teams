00124709a4d57cd5579c4590bbbd57dce0b179f5
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-16 01:56:07 +0200

    Add a function to handle user-related messages sent by the server

--- src/client/messages.c
+++ src/client/messages.c
@@ -27,6 +27,31 @@ bool check_create(char *command)
     return (false);
 }
 
+bool check_user_msg(char *command)
+{
+    for (int i = 0; user_msg[i] != NULL; i++) {
+        if (strcmp(user_msg[i], command) == 0) {
+            return (true);
+        }
+    }
+    return (false);
+}
+
+void manage_user_message(char **msg)
+{
+    char **details = NULL;
+    if (strcmp(msg[0], "USER") == 0){
+        details = split_str(msg[1], '\t');
+        client_print_user(details[0], details[1], atoi(details[2]));
+    }
+    if (strcmp(msg[0], "USERS") == 0){
+        for (int i = 1; msg[i] != NULL; i++){
+            details = split_str(msg[i], '\t');
+            client_print_users(details[0], details[1], atoi(details[2]));
+        }
+    }
+}
+
 void manage_create_message(cteams_t *client, char **msg)
 {
     if (strcmp(msg[0], "TEAM-CREATED") == 0){
@@ -67,11 +92,12 @@ void check_messages(cteams_t *client)
     char **msg = split_str(client->buffer, '\n');
 
     client->tok = strtok(client->buffer, "\r\n");
-    if (check_log(msg[0])){
+    if (check_log(msg[0]))
         manage_log_message(client, msg);
-    } else if (check_create(msg[0])){
+    if (check_create(msg[0]))
         manage_create_message(client, msg);
-    }
+    if (check_user_msg(msg[0]))
+        manage_user_message(msg);
     free(client->buffer);
     client->buffer = malloc(sizeof(char) * 1024 + 1);
 }
