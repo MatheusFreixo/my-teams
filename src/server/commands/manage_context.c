a668f0e1b7f83d69f30a8387d1a19881bea7dc48
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-15 04:13:40 +0200

    Add a function to check the context sent by the client

--- src/server/commands/manage_context.c
+++ src/server/commands/manage_context.c
@@ -7,6 +7,22 @@
 
 #include "./../../../include/teams_server.h"
 
+enum create_type check_context(steams_t *server)
+{
+    if (server->team_id != NULL && server->channel_id != NULL && server->thread_id != NULL){
+        server->context = true;
+        return (REPLY);
+    } else if (server->team_id != NULL && server->channel_id != NULL){
+        server->context = true;
+        return (THREAD);
+    } else if (server->team_id != NULL){
+        server->context = true;
+        return (CHANNEL);
+    }
+    server->context = false;
+    return (TEAM);
+}
+
 void manage_context(steams_t *server, char **command, int client_fd)
 {
     server->team_id = NULL;
@@ -17,7 +33,5 @@ void manage_context(steams_t *server, char **command, int client_fd)
         check_channel(server, command[i]) ? server->channel_id = command[i] : NULL;
         check_thread(server, command[i]) ? server->thread_id = command[i] : NULL;
     }
-    if (server->team_id == NULL && server->channel_id == NULL && server->thread_id == NULL) {
-        server->context = false;
-    }
+    server->create_type = check_context(server);
 }
