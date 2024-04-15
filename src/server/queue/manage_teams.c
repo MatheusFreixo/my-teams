80d25f49bd41395814007b0511794385c5c36cd5
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-15 15:05:31 +0200

    Remove an useless line of code

--- src/server/queue/manage_teams.c
+++ src/server/queue/manage_teams.c
@@ -33,7 +33,6 @@ void add_team_to_list(steams_t *server, char *name, char *desc, int client_fd)
     team->id = gen_uuid_parsed();
     team->name = name;
     team->description = desc;
-    team->channels = NULL;
     if (LIST_EMPTY(&server->teams))
         LIST_INSERT_HEAD(&server->teams, team, entry);
     else
