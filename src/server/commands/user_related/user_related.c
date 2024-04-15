18cb404d874a60d26481365c8ae61ea74c7db6fe
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-16 01:58:49 +0200

    Add a function to handle /users command and anothe to handle /user command

---
+++ src/server/commands/user_related/user_related.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2024
+** my_teams
+** File description:
+** user_related
+*/
+
+#include "./../../../../include/teams_server.h"
+
+void manage_user(steams_t *server, char **command, int client_fd)
+{
+    char *user = NULL;
+    if (command[1] == NULL)
+        return;
+    user = get_specific_user(server, command[1]);
+    if (user == NULL){
+        user = malloc(sizeof(char) * strlen(command[1]) + 13);
+        user = "ERROR-USER\n";
+        strcat(user, command[1]);
+    }
+    write(client_fd, user, strlen(user));
+}
+
+void manage_users(steams_t *server, char **command, int client_fd)
+{
+    char *users = get_all_users(server);
+    write(client_fd, users, strlen(users));
+}
+
+void user_related(steams_t *server, char **command, int client_fd)
+{
+    if (strcmp(command[0], "/users") == 0)
+        manage_users(server, command, client_fd);
+    if (strcmp(command[0], "/user") == 0){}
+        manage_user(server, command, client_fd);
+}
