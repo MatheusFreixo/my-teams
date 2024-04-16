7defd68987574ce466c57326606477e7c631ce2c
Matheus Freixo <matheus.lopez@epitech.eu>
2024-04-16 02:03:21 +0200

    Add conditions to check new commands in check_command function

--- src/server/commands/command_handling.c
+++ src/server/commands/command_handling.c
@@ -12,13 +12,12 @@ int check_command(steams_t *server, char *command, int client_fd)
     char *tok = strtok(server->buffer, "\r\n");
     char **cmd = split_str(command, ' ');
 
-    if (check_log(cmd[0])){
+    if (check_log(cmd[0]))
         manage_log_command(server, cmd[0], cmd[1], client_fd);
-    } else if (strcmp(cmd[0], "/use") == 0){
-        manage_context(server, cmd, client_fd);
-    } else {
-        manage_create_command(server, cmd, client_fd);
-    }
+    if (check_context_cmd(server, cmd[0], client_fd))
+        context_based(server, cmd, client_fd);
+    if (check_user_cmd(server, cmd[0], client_fd))
+        user_related(server, cmd, client_fd);
     free(server->buffer);
     server->buffer = malloc(sizeof(char) * 1024 + 1);
     return (0);
